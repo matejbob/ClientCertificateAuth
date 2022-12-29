b5682652c81f946a1f103cf7d3acdb28ca41bc8f
matej <matej.bobaly@gmail.com>
2022-12-29 11:00:48 +0100

    The custom middleware is plugged in.

--- ClientCertificateAuth/Program.cs
+++ ClientCertificateAuth/Program.cs
@@ -1,7 +1,16 @@
+using ClientCertificateAuth;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-	.ConfigureFunctionsWorkerDefaults()
-	.Build();
+    .ConfigureServices(s =>
+    {
+        s.AddSingleton<CertificateValidator>();
+    })
+    .ConfigureFunctionsWorkerDefaults(workerApplication =>
+    {
+        workerApplication.UseMiddleware<ClientCertificateMiddleware>();
+    })
+    .Build();
 
 host.Run();
